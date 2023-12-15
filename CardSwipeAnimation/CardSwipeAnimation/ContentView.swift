e688f54266c8ff8d5a6b1bd46d7fc3a58f2c9dba
kishanbhadoriya <kbhadouriya05@gmail.com>
2023-12-15 19:53:12 +0530

    Swipe Animation Added

--- CardSwipeAnimation/CardSwipeAnimation/ContentView.swift
+++ CardSwipeAnimation/CardSwipeAnimation/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var backViewSize: CGFloat = 80
+    @State var size: CGSize = .zero
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        GeometryReader{ render in
+            
+            ZStack () {
+                Color.white
+                VStack {
+                    Spacer()
+                    Color.blue
+                }
+                .frame(width: render.size.width-160, height: 300)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(y: -30)
+                
+                VStack {
+                    Color.red
+                }.frame(width: render.size.width-self.backViewSize, height: 300)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(y: -15)
+
+                VStack {
+                    Color.orange
+                }.frame(width: render.size.width-50, height: 300)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .gesture(DragGesture().onChanged({ (value) in
+                    self.size = value.translation
+                    self.backViewSize = 50
+                })
+                .onEnded({ (value) in
+                    self.size = .zero
+                    self.backViewSize = 80
+                }))
+                .offset(self.size)
+            }
+        }
+        .animation(.spring())
     }
 }
 
