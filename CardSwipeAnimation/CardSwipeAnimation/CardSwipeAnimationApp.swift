e21395b51e057e9e39dbd575b63d5e8689ad5c18
kishanbhadoriya <kbhadouriya05@gmail.com>
2023-12-15 16:38:05 +0530

    Add a project file

---
+++ CardSwipeAnimation/CardSwipeAnimation/CardSwipeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardSwipeAnimationApp.swift
+//  CardSwipeAnimation
+//
+//  Created by Kishan on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CardSwipeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
